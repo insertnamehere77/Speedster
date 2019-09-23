9233759909f4da3771f81b23089a07b3aa26b74d
insertnamehere77 <Kaleeb@Kalebs-MacBook-Pro.local>
2019-09-23 16:09:15 -0500

    Cleanup and polish

--- flask_app.py
+++ flask_app.py
@@ -1,60 +1,78 @@
 # encoding=utf8
 import threading, time, json
 import flask
-import slack_bot, game
-
-app = flask.Flask('speedster')
-
-curr_games = {}
+import slack_bot, speedster
 
 
-
+# Bot to talk to slack
 bot = slack_bot.create_config_bot()
 
+# Flask app to listen to slack
+app = flask.Flask('speedster')
+
+# Controls made using block kit for user to interact with
+_LEFT_BUTTON = bot.create_button('⬅️', 'left')
+_RIGHT_BUTTON = bot.create_button('➡️', 'right')
+_CONTROLS = bot.create_actions(_LEFT_BUTTON, _RIGHT_BUTTON)
+
+# All active games
+curr_games = {}
+
+# Time in between updates, needed for Slack rate limiting
+_UPDATE_DELAY = 2
+
+# Top scores to display on game over
+leaderboard = speedster.LeaderBoard()
 
 
-left_button = bot.create_button('⬅️', 'left')
-right_button = bot.create_button('➡️', 'right')
-controls = bot.create_actions(left_button, right_button)
 
-
-# test_game = game.Game()
-# game_str = str(test_game)
-# response_body = bot.send_msg('DN4GLP64B', game_str)
-# response_body = bot.send_msg('DN4GLP64B','', controls)
-
+# Function to run a game until completion
+# Our child threads run this
 def run_game(channel_id, curr_game):
 
-	print('Game starting!')
-
+	# Sending the initial screen and control messages
 	game_str = str(curr_game)
-	response_body = bot.send_msg(channel_id, game_str)
-	bot.send_msg(channel_id, '', controls)
-	timestamp = response_body['ts']
+	screen_resp_body = bot.send_msg(channel_id, game_str)
+	screen_ts = screen_resp_body['ts']
 
+	controls_resp_body = bot.send_msg(channel_id, '', _CONTROLS)
+	controls_ts = controls_resp_body['ts']
+
+
+	# Game loop
 	while curr_game.running:
-		print('Game loop')
-		time.sleep(2)
+		time.sleep(_UPDATE_DELAY)
 		curr_game.update()
 		game_str = str(curr_game)
-		bot.update_msg(channel_id, timestamp, game_str)
+		bot.update_msg(channel_id, screen_ts, game_str)
 
+
+
+	# Game over, post to leaderboard
+	leaderboard.add_score(channel_id, curr_game.score)
+
+	# Display final score and leaderboard
 	game_str = str(curr_game)
-	bot.update_msg(channel_id, timestamp, game_str)
+	bot.update_msg(channel_id, screen_ts, game_str)
+	
+	board_str = str(leaderboard)
+	bot.update_msg(channel_id, controls_ts, board_str)
+
+	# Remove from currently running games
 	del curr_games[channel_id]
 
 
 
-
+# Starts an instance of the game and it's game thread
 def start_game(channel_id):
-	new_game = game.Game()
+	new_game = speedster.Game()
 	curr_games[channel_id] = new_game
 
 	thread = threading.Thread(target = run_game, args = [channel_id, new_game])
 	thread.start()
 
 
-
+# Move the player in the channel's currently running game
 def move_player(channel_id, move):
 	curr_game = curr_games[channel_id]
 
@@ -65,7 +83,7 @@ def move_player(channel_id, move):
 
 
 
-
+# Endpoint to move the player
 @app.route('/move', methods=['POST'])
 def test_move():
 	form = flask.request.form.to_dict()
@@ -81,8 +99,8 @@ def test_move():
 	return '', 200
 
 
-
-@app.route('/', methods=['POST'])
+# Endpoint to start the game
+@app.route('/start', methods=['POST'])
 def test_endpoint():
 	form = flask.request.form.to_dict()
 	channel_id = form['channel_id']
@@ -95,4 +113,5 @@ def test_endpoint():
 
 if __name__ == '__main__':
 	app.run(port = 5000, debug = True)
+
 
---
+++ speedster.py
@@ -0,0 +1,284 @@
+import random, collections
+
+# Represents objects in the game world
+class GameObject():
+
+	# Icon should be the str used to represent obj
+	# Row and Col are where the obj is located on the road
+	def __init__(self, icon, row = 0, col = 0):
+		self.icon = icon
+		self.row = row
+		self.col = col
+
+
+	# Collision detection 
+	# Used the __eq__ function so I can use the "in" keyword
+	def __eq__(self, other):
+		if isinstance(other, GameObject):
+			same_row = (self.row == other.row)
+			same_col = (self.col == other.col)
+			return (same_col and same_row)
+		else:
+			return False
+
+
+
+		
+
+
+# A single running instance of the game
+class Game():
+
+	# Config vars
+	_MAX_HAZARDS = 2
+	_NUM_ROWS = 3
+	_NUM_COLS = 3
+
+	_PLAYER_ICON = '🏎️'
+	_CRASH_ICON = '💥'
+	_HAZARD_ICONS = ['🛢️', '🚘', '🚔', '🚖', '🚍', '🚧']
+
+
+
+	def __init__(self):
+		start_row = self._NUM_ROWS - 1
+		start_col = int(self._NUM_COLS / 2)
+		self._player = GameObject(self._PLAYER_ICON, row = start_row, col = start_col)
+		self._hazards = []
+
+		self.score = 0
+		self.running = True
+		self.crashed = False
+
+
+	# Used to display the game as a string
+	def __str__(self):
+		if self.running:
+			return self._render_game()
+		else:
+			return self._game_over()
+			
+
+	# Updates the game state
+	def update(self):
+
+		# If crashed last update, end game
+		if (self.crashed):
+			self.running = False
+			return
+
+		# If player crashed, update icon
+		if self._player in self._hazards:
+			self.crashed = True
+			self._player.icon = self._CRASH_ICON
+			return
+
+		# Update our current hazards
+		for hazard in list(self._hazards):
+			self._update_hazard(hazard)
+
+		# Spawn more hazards if needed
+		if len(self._hazards) < self._MAX_HAZARDS:
+			self._add_hazard()
+
+
+	# Player moves left
+	def move_left(self):
+		if self.crashed:
+			return
+
+		self._player.col -= 1
+		# Keeping player in bounds
+		if self._player.col < 0:
+			self._player.col = 0
+
+
+
+	# Player moves right
+	def move_right(self):
+		if self.crashed:
+			return
+
+		self._player.col += 1
+		# Keeping player in bounds
+		if self._player.col > (self._NUM_COLS - 1):
+			self._player.col = 2
+
+
+	# Private Methods
+
+
+	# Drawing Functions
+
+	# Returns a string of the currently running game
+	def _render_game(self):
+
+		board = self._create_board()
+
+		# Adding all the hazards to our board
+		for hazard in self._hazards:
+			self._add_board_obj(board, hazard)
+
+		# Adding player
+		self._add_board_obj(board, self._player)
+
+		# Creating the strs for each row
+		row_strs = []
+		for row in board:
+			row_str = self._format_row(row)
+			row_strs.append(row_str)
+
+		# Finally joining the row_strs together
+		return '\n'.join(row_strs)
+
+
+
+	# Returns the str matrix used when displaying the game
+	def _create_board(self):
+		empty_space = '_'
+		board = []
+		for r in range(self._NUM_ROWS):
+			curr_row = []
+
+			for c in range(self._NUM_COLS):
+				curr_row.append(empty_space)
+
+			board.append(curr_row)
+
+		return board
+
+
+
+	# Adds a GameObj to the board matrix
+	def _add_board_obj(self, board, obj):
+		board[obj.row][obj.col] = obj.icon
+
+
+
+	# Formats a board row
+	def _format_row(self, items):
+		row_str = '|{}\t{}\t{}'.format(*items)
+		return row_str
+
+
+
+	# Returns a string of the game over screen
+	def _game_over(self):
+		return 'GAME OVER!\nYour Score: {}'.format(self.score)
+
+
+
+	# Updating functions
+
+
+	# Returns one of the possible hazard icons
+	def _get_hazard_icon(self):
+		icon = random.choice(self._HAZARD_ICONS)
+		return icon
+
+	# Updates the given hazard
+	def _update_hazard(self, hazard):
+
+		# Hazard off screen, gets deleted
+		if hazard.row == 2:
+			self._hazards.remove(hazard)
+			self.score += 1
+
+		# Keeps traversing down the col
+		else:
+			hazard.row += 1
+
+
+	# Adds a new hazard when one has been passed
+	def _add_hazard(self):
+		icon = self._get_hazard_icon()
+		col = random.randrange(self._NUM_COLS)
+		hazard = GameObject(icon, row = 0, col = col)
+
+		self._hazards.append(hazard)
+
+
+
+
+
+
+
+
+
+# Class to hold the high scores across multiple instances of the game
+class LeaderBoard():
+
+	# Config vars
+	_MAX_NUM_SCORES = 3
+
+	# Represents a single score on the board
+	Score = collections.namedtuple('Score', 'name points')
+
+	def __init__(self):
+		self._init_scores()
+		self._sort_scores()
+
+
+	# Adds a new score entry
+	def add_score(self, name, points):
+		score = LeaderBoard.Score(name, points)
+		self._scores.append(score)
+
+		self._sort_scores()
+		self._trim_scores()
+
+
+
+	# Used to display the scores
+	def __str__(self):
+		result = 'TOP SCORES:'
+		curr_place = 1
+		for entry in self._scores:
+			result += '\n'
+			result += '{}. {} \t {}'.format(curr_place, entry.name, entry.points)
+			curr_place += 1
+
+		return result
+
+
+	# PRIVATE METHODS
+
+	# Sets the initial dummy scores
+	def _init_scores(self):
+		self._scores = []
+		vehicles = ['Malibu', 'Lacrosse', 'Denali']
+		curr_score = 10
+
+		for name in vehicles:
+			score = LeaderBoard.Score(name, curr_score)
+			self._scores.append(score)
+			curr_score += 5
+
+
+
+	# Sorts self._scores in descending order
+	def _sort_scores(self):
+		# This lambda returns the key we sort scores by
+		getScore = (lambda x : x.points)
+		self._scores = sorted(self._scores, reverse = True, key = getScore)
+
+
+
+	# Trims our scores to _MAX
+	def _trim_scores(self):
+		self._scores = self._scores[:self._MAX_NUM_SCORES]
+
+
+
+
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
+
